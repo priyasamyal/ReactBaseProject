4504c57b539373c3a12e47bf6f23abab88f6b301
priyasamyal <priyasamyal@gmail.com>
2019-07-22 22:37:17 +0530

    first commit

---
+++ app/components/Home/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { View } from "react-native";
+
+class Home extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Login into applicatio</Text>
+      </View>
+    );
+  }
+}
+
+export default Home;
